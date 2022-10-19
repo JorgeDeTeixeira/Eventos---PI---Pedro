5d5f557a2c08f05fe7ffe9cbde16ef85b46011fb
Jorge Teixeira de Melo <jorge.teixeira@academico.ifrn.edu.br>
2022-10-19 16:13:05 -0300

    aula 3

--- eventos/src/main/java/ifrn/pi/eventos/model/Evento.java
+++ eventos/src/main/java/ifrn/pi/eventos/model/Evento.java
@@ -1,10 +1,28 @@
 package ifrn.pi.eventos.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Evento {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String nome;
 	private String local;
 	private String data;
 	private String horario;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getNome() {
 		return nome;
@@ -36,6 +54,12 @@ public class Evento {
 
 	public void setHorario(String horario) {
 		this.horario = horario;
+	}
+
+	@Override
+	public String toString() {
+		return "Evento [id=" + id + ", nome=" + nome + ", local=" + local + ", data=" + data + ", horario=" + horario
+				+ "]";
 	}
 
 }
