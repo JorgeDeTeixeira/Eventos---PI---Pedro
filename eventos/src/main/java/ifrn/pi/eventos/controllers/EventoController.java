009d98df89b84fb8ad66b86ff1b648263ecfab3c
Jorge Teixeira <jorgedeteixeirademelo@gmail.com>
2022-11-02 17:57:02 -0300

    atv 307 aula 11

--- eventos/src/main/java/ifrn/pi/eventos/controllers/EventoController.java
+++ eventos/src/main/java/ifrn/pi/eventos/controllers/EventoController.java
@@ -3,8 +3,13 @@ package ifrn.pi.eventos.controllers;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,7 +36,11 @@ public class EventoController {
 	}
 
 	@PostMapping
-	public String salvar(Evento evento) {
+	public String salvar(@Valid Evento evento, BindingResult result) {
+		if (result.hasErrors()) {
+			return form(evento);
+		}
+
 		er.save(evento);
 
 		return "redirect:/eventos";
